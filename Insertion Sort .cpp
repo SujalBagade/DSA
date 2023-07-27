8f95e3ed0830bf83bfa3d4265d9a5690091ad6c6
Sujal <107856489+SujalBagade@users.noreply.github.com>
2023-07-27 20:47:56 +0530

    Update Insertion Sort .cpp

--- Insertion Sort .cpp
+++ Insertion Sort .cpp
@@ -1,5 +1,4 @@
-// C++ program for implementation of
-// selection sort
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -15,14 +14,13 @@ void selectionSort(int arr[], int n)
 {
 	int i, j, min_idx;
 
-	// One by one move boundary of
-	// unsorted subarray
+	
 	for (i = 0; i < n-1; i++)
 	{
 	
-		// Find the minimum element in
-		// unsorted array
+		
 		min_idx = i;
+		int a =12;
 		for (j = i+1; j < n; j++)
 		if (arr[j] < arr[min_idx])
 			min_idx = j;
@@ -34,7 +32,7 @@ void selectionSort(int arr[], int n)
 	}
 }
 
-//Function to print an array
+
 void printArray(int arr[], int size)
 {
 	int i;
@@ -43,14 +41,12 @@ void printArray(int arr[], int size)
 	cout << endl;
 }
 
-// Driver program to test above functions
 int main()
 {
 	int arr[] = {64, 25, 12, 22, 11};
 	int n = sizeof(arr)/sizeof(arr[0]);
 	selectionSort(arr, n);
-	cout << "Sorted array: \n";
+	cout<<"Sorted arry is as followws: \n";
 	printArray(arr, n);
 	return 0;
 }
-// This is code is contributed by rathbhupendra
